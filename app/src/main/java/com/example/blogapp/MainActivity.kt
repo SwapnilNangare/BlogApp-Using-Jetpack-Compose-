72e7f737461e23893058b2cdcbfbc460a059cf25
Swapnil Nangare <swapnilnangare143@gmail.com>
2024-07-27 16:11:40 +0530

    Blog App for Theme

---
+++ app/src/main/java/com/example/blogapp/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.blogapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.blogapp.ui.theme.BlogAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            App()
+        }
+    }
+}
+
+
+@Composable
+fun App() {
+
+    var theme = remember { mutableStateOf(false) }
+
+    BlogAppTheme(theme.value) {
+
+        Column(Modifier.background(MaterialTheme.colorScheme.background)) {
+            Text(
+                text = "Swapyy", style = MaterialTheme.typography.headlineLarge
+            )
+
+            Button(onClick = {
+                theme.value = !theme.value
+            }) {
+                Text(text = "Change Theme")
+
+            }
+
+        }
+    }
+
+}
